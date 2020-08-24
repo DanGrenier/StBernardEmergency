5dc81f89e135097f702e14a661259647a06f1d40
Daniel Grenier <dgrenier@smallbizpros.com>
2020-08-24 09:09:47 -0400

    Added internationalization support

--- app/models/diagnosis.rb
+++ app/models/diagnosis.rb
@@ -1,11 +1,11 @@
 class Diagnosis < ApplicationRecord
 	has_many :patient_diagnoses
-	has_many :patients, through: :patient_diagnoses
+	has_many :patients, through: :patient_diagnoses, source: :patient
 	has_many :admission_diagnoses
 	has_many :admissions, through: :admission_diagnoses
 	has_many :patient_chronic_conditions
-	has_many :patients, through: :patient_chronic_conditions
-
+	
+	
 
 	 def description_and_code
   	"#{self.description} (#{self.code})"
